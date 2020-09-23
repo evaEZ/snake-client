7975abbeb8c0e07c385235bca343b771584c9da7
evaEZ <eva.jingzhao@gmail.com>
2020-09-23 03:14:29 +0000

    separate stdin to input.js

--- input.js
+++ input.js
@@ -1,16 +1,19 @@
+
 const setupInput = function(){
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput(data));
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
   return stdin;
 }
 
 const handleUserInput = function(key) {
-  if(key && key.name === "c" && key.ctrl) {
+  if (key === '\u0003'){
     process.exit();
-  }
+  } 
 }
 
 module.exports = {
