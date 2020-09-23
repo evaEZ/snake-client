3c5eaccb69481ce19e632143c29d9a27dfbbbe32
evaEZ <eva.jingzhao@gmail.com>
2020-09-23 03:27:24 +0000

    final version - set the connection

--- client.js
+++ client.js
@@ -38,22 +38,9 @@ const connect = function(){
     }, 50);
 */
   });
+  return conn;
 }
 
-const setupInput = function(){
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput(data));
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if(key && key.name === "c" && key.ctrl) {
-    process.exit();
-  }
-}
 
 module.exports = {
   connect
